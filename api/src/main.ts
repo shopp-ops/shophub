a6b585d7e9d0b052b8aedd3e1208033b48d2c727
Danilo Cvijetic <danilo.cvijetic10@protonmail.ch>
2026-06-05 14:26:25 +0200

    feat: configure Cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3001',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
